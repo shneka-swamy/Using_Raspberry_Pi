0e91738b2d5742aca0081afd929f3d62f8cd089f
shneka-swamy <42727169+shneka-swamy@users.noreply.github.com>
2019-05-14 16:44:13 -0600

    Create test_file.py
    This is to check if a proper communication is set up.

---
+++ test_file.py
@@ -0,0 +1,11 @@
+// This program checks if the serial port communication is set properly between the raspberry pi and the xbee hardware.
+// This program is taken from https://www.raspberrypi.org/forums/viewtopic.php?t=172194
+
+import serial
+import time
+
+ser = serial.Serial("/dev/ttyS0", buadrate= 9600)
+
+while True:
+  data = ser.readline()
+  print(data)
