34dd055b434bf5c81aabd4d0ce39533795249b80
shneka-swamy <42727169+shneka-swamy@users.noreply.github.com>
2019-05-14 16:44:39 -0600

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,5 +1,5 @@
-// This program checks if the serial port communication is set properly between the raspberry pi and the xbee hardware.
-// This program is taken from https://www.raspberrypi.org/forums/viewtopic.php?t=172194
+# This program checks if the serial port communication is set properly between the raspberry pi and the xbee hardware.
+# This program is taken from https://www.raspberrypi.org/forums/viewtopic.php?t=172194
 
 import serial
 import time
