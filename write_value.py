1dca125a813381d72890c9056a996148e6f5ee81
shneka-swamy <42727169+shneka-swamy@users.noreply.github.com>
2019-05-22 18:20:33 -0500

    Update write_value.py

--- write_value.py
+++ write_value.py
@@ -14,7 +14,16 @@ def main():
     # To send the message to a particular user
     remote_device = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string
                                 ("0013A2004102FC32"))
-
+    
+    # This part is included to change the power in the device, once the device power is set it does not change unless specified to
+    # This changes the power of the device in which the program is run
+    # The print statements are included to check for the actual power scale
+   
+    print(device.get_power_level())
+    device.set_power_level(PowerLevel.LEVEL_LOWEST)
+    print(device.get_power_level())
+    
+    # This command is used to send the information from one xbee to another.
     device.send_data(remote_device,"Hello XBee!!")
 
     try:
