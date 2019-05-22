ef4ae1d3799e4f12ac12bc538ec47fe1af8b63c7
shneka-swamy <42727169+shneka-swamy@users.noreply.github.com>
2019-05-22 14:38:00 -0600

    Update read_value.py

--- read_value.py
+++ read_value.py
@@ -1,9 +1,12 @@
-# This program prints the data received from the other device upon receiving an user input
-# This programis used to used to set the basic operation between two xbee s
-# This program is taken from the basic example provided in the digi website
+#This program is used to set communication between two different xbee
+# Connected to a raspberry pi
 
+#This program reads the data at first and then upon receiving an input sends
+#the data to the device it received the data from
 
-from digi.xbee.devices import XBeeDevice
+#This program uses the information provided in the digi website
+
+from digi.xbee.devices import *
 
 def main():
     device = XBeeDevice("/dev/ttyS0", 9600)
@@ -12,6 +15,7 @@ def main():
         device.open()
 
         def data_callback(xbee_message):
+            # Can this address be used in place of the actual MAC Address provided.
             address = xbee_message.remote_device.get_64bit_addr()
             data = xbee_message.data.decode("utf8")
             print("The data %s was received" %data)
@@ -23,8 +27,11 @@ def main():
 
     finally:
         if device is not None and device.is_open():
+            remote_device = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string("0013A2004102FC76"))
+            device.send_data(remote_device, "Hello Partner")
             device.close()
 
 
 if __name__ == '__main__':
     main()
+
