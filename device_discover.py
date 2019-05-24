e8017e11f9bea04d5f574e6149b233260133d56d
shneka-swamy <42727169+shneka-swamy@users.noreply.github.com>
2019-05-24 13:13:48 -0600

    Update device_discover.py
    This program discovers the devices that are present in the vicinity and will send information to them.

--- device_discover.py
+++ device_discover.py
@@ -1,25 +1,32 @@
 # This program is used to discover the devices in the nearby area
-# It does not find itself.
 # This program was taken from the example provided by digi xbee devices documentation
 
 
 import time
 
-from digi.xbee.models.status import NetworkDiscoveryStatus
-from digi.xbee.devices import XBeeDevice
+from digi.xbee.models.status import *
+from digi.xbee.devices import *
 
 def main():
     device = XBeeDevice("/dev/ttyS0", 9600)
+    rem = []
 
     try:
         device.open()
 
         xbee_net = device.get_network()
+        # To check how the RSSI can be received
+        #xbee_net.set_discovery_options({DiscoveryOptions.DISCOVER_MYSELF, DiscoveryOptions.APPEND_RSSI})
+        #xbee_net.set_discovery_options({DiscoveryOptions.APPEND_RSSI})
+
         xbee_net.set_discovery_timeout(15)
         xbee_net.clear()
+        
 
         def callback_device_discovered(remote):
             print("Device discovered is %s" %remote)
+            rem.append(str(remote).split()[0])
+            #send_message(remote)
 
         def callback_discovery_finished(status):
             if status == NetworkDiscoveryStatus.SUCCESS:
@@ -38,6 +45,12 @@ def main():
 
     finally:
         if device is not None and device.is_open():
+            print(rem)
+
+            # This is used to send message to the discovered device
+            remote_device = RemoteXBeeDevice(device,XBee64BitAddress.
+                                             from_hex_string(rem[0]))
+            device.send_data(remote_device,"Hello XBee!!")
             device.close()
 
 if __name__ == '__main__':
