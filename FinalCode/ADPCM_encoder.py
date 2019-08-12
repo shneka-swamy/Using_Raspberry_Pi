bf476dd59df7b9e3526afa18bf539d68f9373cf8
shneka-swamy <42727169+shneka-swamy@users.noreply.github.com>
2019-08-12 12:56:33 -0600

    Update ADPCM_encoder.py

--- FinalCode/ADPCM_encoder.py
+++ FinalCode/ADPCM_encoder.py
@@ -1,41 +1,50 @@
 # This is an implementation of ADPCM encoder.
 # This code is written in reference to Microchip code
 
-
 # This file contains the global variables used in the program
+
+# Now the encoder works for 8 bit, change the code to fit 4 bit and 2 bits
 import globval as gv
 from scipy.io import wavfile
+import numpy as np
 
 converted_file = []
 
 # The list received is of order prevsample, previndex, code
-def send_sample_encoder(check_list):
+def send_sample_encoder(codec, check_list):
 	final_file = [check_list[0]]
 	send_list = [check_list[0], 0, 0]
 
 	for i in range(1, len(check_list)):
-		send_list = ADPCM_encoder(check_list[i], send_list)
+		send_list = ADPCM_encoder(codec, check_list[i], send_list)
 		#print(send_list)
 		final_file.append(send_list[2])
 
 	return final_file
 
-def inverse_quantizer(code, step):
+def inverse_quantizer(codec, code, step):
 	# Inverse quantize the ADPCM code into predicted difference using the quantizer
 	# step size
-	diffq = step >> 3
-	if code & 4:
+
+	if codec == 4:
+		diffq = step >> 3
+	elif codec == 3:
+		diffq = step >> 2
+	else:
+		diffq = step >> 1
+
+	if (codec == 4 and code & 4) or (codec == 3 and code & 2) or (codec == 2 and code & 1):
 		diffq += step
-	if code & 2:
+	if (codec == 4 and code & 2) or (codec == 3 and code & 1):
 		diffq += step >> 1
-	if code & 1:
+	if (codec == 4 and code & 1):
 		diffq += step >> 2
 
 	return diffq
 
-def get_predicted_value(predsample, code, diffq):
+def get_predicted_value(codec, predsample, code, diffq):
 	# Find the new predicted value by adding or subtracting the diffq
-	if code & 8:
+	if (codec == 4 and code & 8) or (codec == 3 and code & 4) or (codec == 2 and code & 2):
 		predsample -= diffq
 	else:
 		predsample += diffq
@@ -48,9 +57,15 @@ def get_predicted_value(predsample, code, diffq):
 
 	return predsample
 
-def get_index_value(index, code):
+def get_index_value(codec, index, code):
 	# Find the new stepsize and index value
-	index += gv.IndexTable[code]
+	if codec == 4:
+		index += gv.IndexTable_8[code]
+	elif codec == 3:
+		index += gv.IndexTable_4[code]
+	else:
+		index += gv.IndexTable_2[code]
+
 	if index < 0:
 		index = 0
 	if index > 88:
@@ -59,7 +74,7 @@ def get_index_value(index, code):
 	return index
 
 
-def ADPCM_encoder(sample, send_list):
+def ADPCM_encoder(codec, sample, send_list):
 	
 	# To get the previous values set as a variable
 	predsample = send_list[0]
@@ -72,54 +87,82 @@ def ADPCM_encoder(sample, send_list):
 	if diff >= 0:
 		code = 0
 	else:
-		code = 8
+
+		if codec == 4:
+			code = 8
+		elif codec  == 3:
+			code = 4
+		elif codec == 2:
+			code = 2
+
 		diff = -diff
 	
 	# Quantize the difference into 4 bit ADPCM code using the quanitzer step size
 	tempstep = step
 	if diff >= tempstep:
-		code |= 4
+		
+		if codec == 4:
+			code |= 4
+		elif codec == 3:
+			code |= 2
+		elif codec == 2:
+			code |= 1
+
 		diff -= tempstep
 	
 	tempstep >>= 1
 	if diff >= tempstep:
-		code |= 2
+		
+		if codec == 4:
+			code |= 2
+		elif codec == 3:
+			code |= 1
+
 		diff -= tempstep
 
 	tempstep >>= 1
 	if diff >= tempstep:
-		code |= 1
 
-	diffq = inverse_quantizer(code, step)
-	predsample = get_predicted_value(predsample, code, diffq)
-	index = get_index_value(index, code)
+		if codec == 4:
+			code |= 1
 
-	send_list = [predsample, index, (code & 0x0f)]
+	diffq = inverse_quantizer(codec, code, step)
+	predsample = get_predicted_value(codec, predsample, code, diffq)
+	index = get_index_value(codec, index, code)
+
+	if codec == 4:
+		code &= 0x0f
+	elif codec == 3:
+		code &= 0x07
+	else:
+		code &= 0x03
+
+	send_list = [predsample, index, code]
 
 	return send_list
 
-def send_sample_decoder(check_list):
+def send_sample_decoder(codec, check_list):
 	original_file = [check_list[0]]
 	received_list = [check_list[0], 0]
 	#print(received_list)
 
 	for i in range(1, len(check_list)):
-		received_list = ADPCM_decoder(check_list[i], received_list)
+		received_list = ADPCM_decoder(codec, check_list[i], received_list)
 		#print(received_list)
 		original_file.append(received_list[0])
 
 	return original_file
 
 
-def ADPCM_decoder(code, send_list):
+def ADPCM_decoder(codec, code, send_list):
 	predsample = send_list[0]
 	index = send_list[1]
 
 	step = gv.StepSizeTable[index]
 
-	diffq = inverse_quantizer(code, step)
-	predsample = get_predicted_value(predsample, code, diffq)
-	index = get_index_value(index, code)
+	diffq = inverse_quantizer(codec, code, step)
+	predsample = get_predicted_value(codec, predsample, code, diffq)
+	index = get_index_value(codec, index, code)
 
 	send_list = [predsample, index]
 
@@ -128,19 +171,26 @@ def ADPCM_decoder(code, send_list):
 
 def main():
 	
-	#fs, data = wavfile.read('first.wav', 'b')
+	fs, data = wavfile.read('first.wav', 'b')
 	
-	#print(data.size)
+	print(data.size)
+
+	codec = 2
 	
-	check_list = [150, 155, 167, 170, 250, 250, 250, 250, 200]
+	#check_list = [150, 155, 167, 170, 250, 250, 250, 250, 200]
 
 	#check_list = [150, 155]
 
-	converted_file = send_sample_encoder(check_list)
-	print(converted_file)
+	converted_file = send_sample_encoder(codec, data)
 
-	original_file = send_sample_decoder(converted_file)
-	print(original_file)
+	original_file = send_sample_decoder(codec, converted_file)
+	original_file.append(fs)
+
+	fs_new = original_file[-1]
+	final_list = original_file[:-1]
+	received = np.asarray(final_list).astype(np.int16)
+	
+	wavfile.write('new_first.wav', fs_new, received)
 
 
 
