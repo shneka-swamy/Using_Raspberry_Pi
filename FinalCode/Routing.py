cc2665091bc18a286e9f18c2db50d8e9abb9f6bb
shneka-swamy <42727169+shneka-swamy@users.noreply.github.com>
2019-08-27 17:32:06 -0600

    Update Routing.py

--- FinalCode/Routing.py
+++ FinalCode/Routing.py
@@ -1,7 +1,11 @@
 
 # This is the program written to enable routing
 # The program assumes that the source and the receivers are known
-# The source and destination will always have [0,0,0,0]
+# The source and destination will always have [0,0,0,0,1]
+
+# Multiple sources can be set.. How to set multiple sinks ?
+# Make the destination a list of lists ?
+
 
 import graph as gp
 
@@ -10,10 +14,11 @@ class Nodes:
 
 	def __init__(self, arg):
 		# The message list considers the required message
-		# [source, destination, Effective Degree, Host]  
+		# [source, destination, Effective Degree, Hop, Mode of the Node]  
+		# The modes of the nodes are defined as 0: Relay, 1: Source and 2: Destination
 		# The message list also contains the nearby neighbours
 		# The elements in the adj list (are appended when the member is visited)
-		self.message_list = [arg]*4
+		self.message_list = [arg]*5
 	
 	def set_source_dest(self, source, dest):
 		self.message_list[0] = source
@@ -29,6 +34,17 @@ class Nodes:
 	def erase_list(self, source):
 		self.message_list = self.message_list[0:4]
 
+# Path is the list that contains all the paths
+# node is the element adjacent to the list
+def get_path(path, node, NodeList):
+	create_path = [(gp.destination, 0)]
+	dest = gp.destination
+	
+	while dest != gp.source:
+		create_path.append((NodeList[dest].message_list[0], NodeList[dest].message_list[2]))
+		dest = NodeList[dest].message_list[0]
+
+	path.append(create_path)
 
 
 def main():
@@ -43,7 +59,19 @@ def main():
 	# The value of source will always be -1
 	
 	stack = [gp.source]
+
+	# To assign oneself as source or destination
+	NodeList[gp.source].message_list[4] = 1
+	NodeList[gp.destination] .message_list[4] = 2
+
+	source_include = 3
+	NodeList[source_include].message_list[4] = 1
+
+	source_include = 5
+	NodeList[source_include].message_list[4] = 1
+
 	distances = []
+	path = []
 
 	while len(stack) != 0:
 		# The stack contains the neighbours that must be looked at
@@ -54,23 +82,29 @@ def main():
 
 		for node in copy_stack:
 			if gp.destination in gp.adj_list[node]:
+				NodeList[gp.destination].set_source_dest(node, gp.destination)
+				NodeList[gp.destination].change_value(node, NodeList[node].message_list[3] + 1)
 				distances.append(NodeList[node].message_list[3] + 1)
+				print("In the path finding")
+				get_path(path, node, NodeList)
+				print(path)
 			
 			else:
 				if node == gp.source:
 					# Broadcast the message to all elements in te adj list
 					for member in gp.adj_list[node]:
-						NodeList[member].set_source_dest(node, gp.destination)
-						NodeList[member].change_value(node, NodeList[node].message_list[3] + 1)
-						NodeList[member].append_neighbours(node)
-						stack.append(member)
+						if NodeList[member].message_list[4]!=1:
+							NodeList[member].set_source_dest(node, gp.destination)
+							NodeList[member].change_value(node, NodeList[node].message_list[3] + 1)
+							NodeList[member].append_neighbours(node)
+							stack.append(member)
 					print(stack)
 
 				else:
 
 					for member in gp.adj_list[node]:
-						if member != NodeList[node].message_list[0]:
-							if NodeList[member].message_list[3] == 0:
+						if member != NodeList[node].message_list[0]: 
+							if NodeList[member].message_list[3] == 0 and NodeList[member].message_list[4] != 1:
 								NodeList[member].set_source_dest(node, gp.destination)
 								NodeList[member].change_value(node, NodeList[node].message_list[3] + 1)
 								NodeList[member].append_neighbours(node)
