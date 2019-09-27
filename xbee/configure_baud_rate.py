8723d3b16f3a4b1cb7735cd0e5f683446c014460
rwarner <rwarner@mymail.mines.edu>
2019-09-26 21:07:35 -0600

    Fixed errors

--- xbee/configure_baud_rate.py
+++ xbee/configure_baud_rate.py
@@ -8,20 +8,24 @@ from digi.xbee.exception import *
 
 
 class Xbee_Initialization:
-    def __init__(self, port_name='/dev/ttyUSB0', baud_rate=115200, timeout=1):
-        with serial.Serial(port_name, baud_rate, timeout=timeout) as self.ser:
-            if self._set_xbee_max_baud_():
+    def __init__(self, port_name='/dev/ttyUSB0', baud_rate=115200, timeout=2.5):
+        self.port_name = port_name
+        self.timeout=timeout
+        with serial.Serial(port_name, baud_rate, timeout=self.timeout) as self.ser:
+            if self._set_xbee_max_baud():
+                print('115200')
                 baud_rate = 250000
                 self.ser.baudrate = baud_rate
             else:
-                if self._test_max_baud_rate_():
+                print('NOT 115200')
+                if self._test_max_baud_rate():
                     baud_rate = 250000
                     self.ser.baudrate = baud_rate
                 else:
                     print("Wrong baud rate!")
                     return
             
-            if self._enable_api_mode_():
+            if self._enable_api_mode():
                 self.xbee = XBeeDevice(port_name, baud_rate)
                 try:
                     self.xbee.open()
@@ -31,59 +35,79 @@ class Xbee_Initialization:
                 print("Error Entering API mode")
                 return None
 
-            
+
     def getXbeeDevice(self):
         return self.xbee
 
-    def _verify_response_(self, time_delay):
-        time.sleep(time_delay)
+    def get_baud_rate(self, baud_rate):
+        with serial.Serial(self.port_name, baud_rate, timeout=self.timeout) as self.ser:
+            self.ser.write(b'+++')
+            
+            if not self._verify_response():
+                return False
+
+            self.ser.write(b'ATBD\r')
+            print(self.ser.readline())
+            self.ser.write(b'ATCN\r')
+            if not self._verify_response():
+                return False
+
+
+
+    def _verify_response(self):
         line = self.ser.readline()
         print(line)
         return b'OK' in line
 
-    def _set_xbee_max_baud_(self):
+    def _set_xbee_max_baud(self):
         self.ser.write(b'+++')
         
-        if not self._verify_response_(2):
-            return False
-
-        self.ser.write(b'ATWR\r')
-        if not self._verify_response_(.5):
+        if not self._verify_response():
             return False
 
         self.ser.write(b'ATBD3D090\r')
-        if not self._verify_response_(.5):
+        if not self._verify_response():
             return False
 
+
         self.ser.write(b'ATAC\r')
-        if not self._verify_response_(.5):
-            return False 
+        if not self._verify_response():
+            return False     
+
+        self.ser.write(b'ATWR\r')
+        if not self._verify_response():
+            return False
 
         self.ser.write(b'ATCN\r')
         time.sleep(5)
         return True
     
-    def _enable_api_mode_(self):
+    def _enable_api_mode(self):
         self.ser.write(b'+++')
-        if not self._verify_response_(2):
+        if not self._verify_response():
             return False 
 
-        self.ser.write(b'ATAP2\r')
-        if not self._verify_response_(.5):
+        self.ser.write(b'ATAP1\r')
+        if not self._verify_response():
             return False 
+        
+        self.ser.write(b'ATWR\r')
+        if not self._verify_response():
+            return False
+
         self.ser.write(b'ATAC\r')
-        if not self._verify_response_(.5):
+        if not self._verify_response():
             return False 
         self.ser.write(b'ATCN\r')
 
         return True        
 
-    def _test_max_baud_rate_(self):
-        start_baudrate = self.ser.baudrate
+    def _test_max_baud_rate(self):
+        start_baudrate = self.ser.baudrate 
         self.ser.baudrate = 250000
 
         self.ser.write(b'+++')        
-        message_success = self._verify_response_(1.5)
+        message_success = self._verify_response()
         if message_success:
              self.ser.write(b'ATCN\r')
              time.sleep(2)
