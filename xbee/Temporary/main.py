90ddafdfd5d43b1a94a62bcd4c9839d1ae660895
Ethan <Eperry1@mymail.mines.edu>
2019-10-09 09:50:29 -0600

    Updated Sender and Receiver methods

--- xbee/Temporary/main.py
+++ xbee/Temporary/main.py
@@ -2,7 +2,7 @@ import Send_Audio
 
 def main():
     Send_Audio.hello()
-    Send_Audio.send_audio(100, "../../AudioFiles/CantinaBand3.wav")
+    Send_Audio.send_audio(10, "../../AudioFiles/CantinaBand3.wav")
 
 if __name__ == '__main__':
     main()
